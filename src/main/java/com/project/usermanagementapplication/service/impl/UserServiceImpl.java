daa3329988dc2765c65fafa6ead906c2b5463ba7
Satyamsahugr8 <satyamsahugr8@gmail.com>
2024-09-02 21:03:36 +0530

    updated service file

--- src/main/java/com/project/usermanagementapplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/usermanagementapplication/service/impl/UserServiceImpl.java
@@ -1,7 +1,8 @@
-package com.project.usermanagementapplication.service;
+package com.project.usermanagementapplication.service.impl;
 
 import com.project.usermanagementapplication.entity.User;
 import com.project.usermanagementapplication.repository.UserRepository;
+import com.project.usermanagementapplication.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,26 +10,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService{
-	
+public class UserServiceImpl implements UserService {
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Override
 	public User saveUser(User user) {
 		return userRepository.save(user);
 	}
 
-    @Override
+	@Override
 	public List<User> getAllUsers() {
 		return (List<User>) userRepository.findAll();
 	}
-    
+
 	@Override
 	public User getUserById(@RequestBody int userId) {
 		return userRepository.findById(userId).get();
 	}
-	
+
 	@Override
 	public void deleteUser(User existingUser) {
 		userRepository.delete(existingUser);
